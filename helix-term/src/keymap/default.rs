b66cb2c5145c9fb1478e0bb89e90bcd426ada431
Arman Soltani <armansoltani@protonmail.com>
2024-12-18 12:02:47 +0100

    Add command to display bookmarks picker

--- helix-term/src/keymap/default.rs
+++ helix-term/src/keymap/default.rs
@@ -221,6 +221,7 @@ pub fn default() -> HashMap<Mode, KeyTrie> {
 
         "space" => { "Space"
             "f" => file_picker,
+            "m" => goto_bookmark,
             "F" => file_picker_in_current_directory,
             "e" => file_explorer,
             "E" => file_explorer_in_current_buffer_directory,
