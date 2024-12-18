1e854df9e2e20719674b5286616ed0e634fdbc4c
Arman Soltani <armansoltani@protonmail.com>
2024-12-18 14:52:11 +0100

    Remove bookmarks pointing to files that do not exist anymore

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -3,6 +3,7 @@ use std::io::BufReader;
 use std::io::Write as _;
 use std::ops::Deref;
 
+use crate::actualize_bookmarks;
 use crate::job::Job;
 
 use super::*;
@@ -2443,6 +2444,7 @@ fn clear_bookmark(
             .lines()
             .map(|line| serde_json::from_str(line).unwrap())
             .collect();
+        let bookmarks = actualize_bookmarks(bookmarks);
         let n = bookmarks.len();
 
         let bookmarks_update: Vec<_> = bookmarks
