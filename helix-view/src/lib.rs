7c94898612c5d1222cc3a10c5732c9137f9d5cec
Arman Soltani <armansoltani@protonmail.com>
2024-12-24 18:00:48 +0100

    Display bookmarks as fake lsp diagnostics

--- helix-view/src/lib.rs
+++ helix-view/src/lib.rs
@@ -75,7 +75,8 @@ pub fn align_view(doc: &mut Document, view: &View, align: Align) {
 
 pub use document::Document;
 pub use editor::Editor;
-use helix_core::{char_idx_at_visual_offset, uri::actualize_bookmarks, BookmarkUri};
+use helix_core::{char_idx_at_visual_offset, uri::actualize_bookmarks, BookmarkUri, Diagnostic};
+use helix_lsp::LanguageServerId;
 pub use theme::Theme;
 pub use view::View;
 
@@ -122,4 +123,81 @@ pub fn read_and_update_bookmarks_cache(
         })
         .unwrap_or_default();
     actualize_bookmarks(bookmarks)
+}
+
+pub fn read_and_update_document_bookmarks_cache(doc: &Document) -> Vec<BookmarkUri> {
+    let mut bookmark_file_path = helix_stdx::env::current_working_dir();
+    bookmark_file_path.push(".bookmarks");
+    let bookmark_file_path = bookmark_file_path.as_path().to_string_lossy().to_string();
+
+    if let Some(doc_path) = doc
+        .path()
+        .map(|p| p.as_path().to_string_lossy().to_string())
+    {
+        if doc.bookmarks_cache.borrow().is_none() {
+            // read bookmarks from file and update the cache
+            log::info!("reading bookmark file from disk");
+
+            let bookmarks_data = std::fs::read_to_string(bookmark_file_path).unwrap_or("".into());
+            let bookmarks: Vec<BookmarkUri> = bookmarks_data
+                .lines()
+                .filter(|line| !line.is_empty())
+                .map(|l| serde_json::from_str(l).unwrap())
+                .collect();
+
+            let mut new_bookmarks_cache: HashMap<usize, BookmarkUri> = HashMap::new();
+
+            for bookmark in bookmarks {
+                if bookmark.path != doc_path {
+                    continue;
+                }
+
+                new_bookmarks_cache.insert(bookmark.line, bookmark);
+            }
+
+            *doc.bookmarks_cache.borrow_mut() = Some(new_bookmarks_cache);
+        }
+
+        doc.bookmarks_cache
+            .borrow()
+            .clone()
+            .map(|cache| cache.values().cloned().collect())
+            .unwrap_or_default()
+    } else {
+        vec![]
+    }
+}
+
+pub fn convert_bookmarks_to_fake_diagnostics(
+    doc: &Document,
+    bookmarks: Vec<BookmarkUri>,
+) -> Vec<Diagnostic> {
+    let mut diagnostics = vec![];
+
+    for bookmark in bookmarks {
+        let fake_lsp_diagnostics = helix_lsp::lsp::Diagnostic::new_simple(
+            helix_lsp::lsp::Range::new(
+                helix_lsp::Position {
+                    line: bookmark.line as u32,
+                    character: 0,
+                },
+                helix_lsp::Position {
+                    line: bookmark.line as u32,
+                    character: 0,
+                },
+            ),
+            bookmark.name,
+        );
+        let diagnostic = Document::lsp_diagnostic_to_diagnostic(
+            doc.text(),
+            None,
+            &fake_lsp_diagnostics,
+            LanguageServerId::default(),
+            helix_lsp::OffsetEncoding::default(),
+        )
+        .unwrap();
+        diagnostics.push(diagnostic);
+    }
+
+    diagnostics
 }
